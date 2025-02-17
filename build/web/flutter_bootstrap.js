629cf92ea21d52ab81c8ed51cab45c84758595ec
linggommarpaung <linggommarpaung31@gmail.com>
2025-02-17 11:59:33 +0100

    Changing the main icon

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1090704148"
+    serviceWorkerVersion: "1455156589"
   }
 });
