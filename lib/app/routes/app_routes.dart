fa6cb1899cdf4c8b419a014e4089746ac930d0d4
linggommarpaung <linggommarpaung31@gmail.com>
2025-02-25 20:19:31 +0100

    ä

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -27,7 +27,7 @@ abstract class AcnooAppRoutes {
           if (state.uri.queryParameters['rtl'] == 'true') {
             _appLangProvider.isRTL = true;
           }
-          return 'dashboard/earning-admin';
+          return '/dashboard/ecommerce-admin';
         },
       ),
 
